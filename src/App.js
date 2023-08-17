1f0dd35cff49c64db5ef405b1177c49b2f7852db
phinsso <naso01247@gmail.com>
2023-08-18 00:19:46 +0900

    (1.6) 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 
-// props: 어떠한 값을 컴포넌트에 전달해줘야 할 때 사용함.
+// 조건부 렌더링: 특정 조건에 따라 다른 결과물을 렌더링하는 것
 
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
-      
-      {/* name이 없는 컴포넌트(defaultProps) 렌더링 */}
+      {/* props 이름만 작성하고 값 설정 생략 => true 설정으로 간주
+          ex) <Hello name="react" color="red" isSpecial />*/}
+      <Hello name="react" color="red" isSpecial={true}/>
       <Hello color="pink" />
     </Wrapper>
   );
--- src/Hello.js
+++ src/Hello.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-// Hello 컴포넌트에서 name값을 사용
-
-// props는 객체 형태로 전달됨
-/* function Hello(props) {
-    return <div style={{ color: props.color }}>안녕하세요 {props.name}</div>
-  } */
-
-function Hello({color, name}) {
-    // 함수의 파라미터에서 '비구조화 할당' 문법을 사용하면 코드가 간결해짐
-    return <div style={{ color }}>안녕하세요 {name}</div>
+function Hello({ color, name, isSpecial }) {
+    return (
+        // isSpecial의 값이 true -> 컴포넌트의 좌측에 * 표시되도록
+        <div style={{ color }}>
+            {isSpecial && <b>*</b>}
+            안녕하세요 {name}
+        </div>
+    );
 }
 
-// 컴포넌트에 props를 지정하지 않았을 때 기본적으로 사용할 값 설정
 Hello.defaultProps = {
     name: '이름없음'
 }
