c15050cf5a2fc26567d8cb7602966912bb3a8e21
phinsso <naso01247@gmail.com>
2023-08-19 17:40:08 +0900

    (1.9) 여러개의 input 상태 관리하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 
-/* input의 onChange를 사용하면 이벤트 객체 e를 파라미터로 받아올 수 있다.
-이 객체의 e.target은 이벤트가 발생한 DOM을 가리킨다.
-e.target.value를 조회하면 현재 input의 value값을 알 수 있다. */
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
   };
 
   const onReset = () => {
-    setText('');
+    setInputs({
+      name: '',
+      nickname: '',
+    })
   };
+
 
   return (
     <div>
-      <input onChange={onChange} value={text}  />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
