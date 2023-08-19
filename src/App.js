36055717e4a1159298e897cb5a19983f1f1565e7
phinsso <naso01247@gmail.com>
2023-08-19 17:50:21 +0900

    (1.12) useRef로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
-import InputSample from "./InputSample";
+import React, { useRef } from 'react';
+import UserList from './UserList';
+
+/* useRef의 또 다른 역할
+    - 컴포넌트 안에서 조회 및 수정 할 수 있는 변수 관리
+    - useRef로 관리되는 변수는 값이 바뀌어도 컴포넌트가 리렌더링 되지 않는다.*/
 
 function App() {
-  return (
-    <InputSample />
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,9 +1,4 @@
 import React from 'react';
-// arr.map(i => ) 의 형태로 하위 컴포넌트에게 값을 전달해준다.
-// 리액트에서 배열을 렌더링 할 때에는 key 라는 props 를 설정해야 함.
-
-/* Map에 key 값이 없다면 중간의 값이 바뀌었을 때 그 하위 값들이 전부 변하게 된다.
-key값을 사용한다면 key를 이용해 중간의 값을 추가하게 된다.*/
 
 function User({ user }) {
   return (
@@ -13,25 +8,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
-
+function UserList({ users }) {
   return (
     <div>
       {users.map(user => (
